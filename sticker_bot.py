c93b3723d9f9b9b991b86981ca5f7d0084cef322
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2024-04-20 21:30:15 +0530

    feat: autocomplete cmd

--- sticker_bot.py
+++ sticker_bot.py
@@ -1,11 +1,15 @@
 import discord
 from discord.ext import commands
-import os
+from discord import app_commands
 from PIL import Image
+from utils.env import get_env
+import os
+import io
 
 intents = discord.Intents.default()
 intents.guilds = True
 intents.messages = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
@@ -13,23 +17,18 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 async def send_sticker(ctx):
     sticker_path = 'stickers/putin.png'
     
-    # Open the image
     with open(sticker_path, 'rb') as f:
         img = Image.open(f)
         
-        # Resize the image to 128x128
         img = img.resize((128, 128))
         
-        # Save the resized image to a temporary file
         temp_path = 'temp_sticker.png'
         img.save(temp_path)
         
-        # Send the resized image
         with open(temp_path, 'rb') as resized_f:
             resized_sticker = discord.File(resized_f)
             await ctx.send(file=resized_sticker)
     
-    # Delete the temporary file
     os.remove(temp_path)
 
 @bot.command()
@@ -43,7 +42,23 @@ async def lolhelp(ctx):
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
     print(f'Bot ID: {bot.user.id}')
+    synced = await bot.tree.sync()
+    print('synced command(s)', synced)
+
+@bot.tree.command(name='sticker', description='Send a sticker')
+@app_commands.describe(sticker='Sticker name')
+async def sticker_putin(interaction: discord.Interaction, sticker: str):
+    with open(f'putin/{sticker}.png', 'rb') as f:
+        img = Image.open(f)
+        img = img.resize((128, 128), Image.BICUBIC)
+
+        img.save('test.png', 'PNG')
+
+    
+    await interaction.response.send_message(file=discord.File('temp.png'))
+
+    os.remove('temp.png')
 
 
-bot.run(os.getenv('DISCORD_TOKEN'))
+bot.run(get_env('DISCORD_TOKEN'))
 
