3cb0331d4f1a54f64b40026a152e023d619222ad
Nilotpaul Nandi <nilotpaul.nandi2021@uem.edu.in>
2024-04-20 21:43:07 +0530

    feat: comments

--- sticker_bot.py
+++ sticker_bot.py
@@ -11,53 +11,35 @@ intents.guilds = True
 intents.messages = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intents)
+bot = commands.Bot(command_prefix='/', intents=intents) # client bot initialization
 
-@bot.command()
-async def send_sticker(ctx):
-    sticker_path = 'stickers/putin.png'
-    
-    with open(sticker_path, 'rb') as f:
-        img = Image.open(f)
-        
-        img = img.resize((128, 128))
-        
-        temp_path = 'temp_sticker.png'
-        img.save(temp_path)
-        
-        with open(temp_path, 'rb') as resized_f:
-            resized_sticker = discord.File(resized_f)
-            await ctx.send(file=resized_sticker)
-    
-    os.remove(temp_path)
-
-@bot.command()
+@bot.command() # help commands
 async def lolhelp(ctx):
     embed = discord.Embed(title="Bot Commands", description="List of available commands:", color=0x00ff00)
-    embed.add_field(name="/send_sticker", value="Send a sticker", inline=False)
+    embed.add_field(name="/sticker:{name}", value="Send a sticker", inline=False)
     embed.add_field(name="/lolhelp", value="Show all commands", inline=False)
     await ctx.send(embed=embed)    
 
 @bot.event
-async def on_ready():
+async def on_ready(): # on successfull connection
     print(f'{bot.user.name} has connected to Discord!')
     print(f'Bot ID: {bot.user.id}')
-    synced = await bot.tree.sync()
+    synced = await bot.tree.sync() # sync commands on every connect
     print('synced command(s)', synced)
 
-@bot.tree.command(name='sticker', description='Send a sticker')
+@bot.tree.command(name='sticker', description='Send a sticker') # sticker command autocomplete
 @app_commands.describe(sticker='Sticker name')
 async def sticker_putin(interaction: discord.Interaction, sticker: str):
     with open(f'putin/{sticker}.png', 'rb') as f:
         img = Image.open(f)
-        img = img.resize((128, 128), Image.BICUBIC)
+        img = img.resize((128, 128), Image.BICUBIC) # resizing and saving temp img locally
 
-        img.save('test.png', 'PNG')
+        img.save('temp.png', 'PNG')
 
     
-    await interaction.response.send_message(file=discord.File('temp.png'))
+    await interaction.response.send_message(file=discord.File('temp.png')) # uploading
 
-    os.remove('temp.png')
+    os.remove('temp.png') # removing the temp img
 
 
 bot.run(get_env('DISCORD_TOKEN'))
